74cbcda09a5436679d5a905c01e5fe6304318b2f
prusovak2 <prusova.k2@seznam.cz>
2020-04-01 16:22:52 +0200

    attempts to resample data

--- ReadAndJoin.py
+++ ReadAndJoin.py
@@ -1,37 +1,63 @@
 import pandas as pd
+import numpy as np
 
-#TODO: find out what columns represent and rename them properly
-cmdsColumnNames = ['time', 'cmd1', 'cmd2', 'cmd3', 'cmd4']
+cmdsColumnNames = ['time', 'leftRight', 'frontBack', 'up', 'angular']
 cmds = pd.read_csv('commands.tsv', delimiter='\t', header=None, names=cmdsColumnNames)
-cmds = cmds.set_index('time')
+#cmds = cmds.set_index('time')
+print("unique cmds time:", cmds.time.is_unique)
 
-#TODO: how the time is represented? miliseconds since when?
-#cmds.index = pd.to_datetime(cmds.index)
+navDataColumnNames = ['time', 'State', 'Battery_level', 'Magnetometer_x', 'Magnetometer_y', 'Magnetometer_z', 'Pressure',
+                      'Temperature', 'Wind_speed', 'Wind_angle', 'Wind_compensation:pitch', 'Wind_compensation:roll',
+                      'Pitch_y', 'Roll_x', 'Yaw_z', 'Altitude', 'Velocity_x',
+                      'Velocity_y', 'Velocity_z', 'Acceleration_x', 'Acceleration_y', 'Acceleration_z',
+                      'Motor_1_power', 'Motor_2_power', 'Motor_3_power', 'Motor_4_power', 'time_board']
 
-navDataColumnNames = ['time', 'State', 'Battery level', 'Magnetometer x', 'Magnetometer y', 'Magnetometer z', 'Pressure',
-                      'Temperature', 'Wind speed', 'Wind angle', 'Wind compensation: pitch', 'Wind compensation: roll',
-                      'Pitch (Rotation in y)', 'Roll (Rotation in x)', 'Yaw (Rotation in z)','Altitude', 'Velocity in x',
-                      'Velocity in y', 'Velocity in z', 'Acceleration in x', 'Acceleration in y', 'Acceleration in z',
-                      'Motor 1 power', 'Motor 2 power', 'Motor 3 power', 'Motor 4 power', 'time board']
-#TODO: what does second time (last column) mean?
 navData = pd.read_csv('navdata.tsv', delimiter='\t', header=None, usecols=range(1, 28), names=navDataColumnNames)
-navData = navData.set_index('time')
+print("unique navdata time:", navData.time.is_unique)
+#get rid of duplicated times in navData
+navData.drop_duplicates(['time'], keep='first', inplace=True)
+print("unique navdata time after .drop_duplicated call:", navData.time.is_unique)
+#navData = navData.set_index('time')
 
-#TODO: how? inner - intersection, does it make sence to keep times that does not have corresponding counterpart in second file?
-merged = pd.merge(cmds, navData, right_index=True, left_index=True, how='inner')
+# how inner - intersection, keeps only times that do have corresponding counterpart in second file
+#merged = pd.merge(cmds, navData, right_index=True, left_index=True, how='inner') #merge when time is index
+merged = pd.merge(cmds, navData, right_on="time", left_on="time", how='inner', indicator=True)
 
-merged.to_csv('output.tsv', sep='\t')
+#TODO:How to get rid of duplicated times in navdata?
+merged.to_csv("merged.tsv", sep='\t')
+print("unique time right after merge:", merged.time.is_unique)
+
+#substract minimal time from all times - get rid of miliseconds since start of unix
+minTime = merged.time.min()
+merged.time = merged.time-minTime
+
+merged.to_csv("mergedTime.tsv", sep='\t')
+
+#to convert 'time' to datetime format
+dateIndex = pd.to_datetime(merged.time, unit='ms')
+
+indexedByTime = merged.set_index('time')
+indexedByTime.index = dateIndex
+#TODO: after this data is indexed by senceless dates begginning with start of unix date - does it make any sence?
+#TODO: can I somehow format string representation of values in particular columns while printing them to file by to_scv
+#TODO: to print 'time' in some meaningfull format
+
+indexedByTime.to_csv('output.tsv', sep='\t')
+
+print("unique indexed by time:", indexedByTime.index.is_unique)
+
+#resampled = indexedByTime.resample('5s').interpolate()
+
+#resample_index = pd.date_range(start=indexedByTime.index[0], end=indexedByTime.index[-1], freq='50ms')
+#dummy_frame = pd.DataFrame(np.NaN, index=resample_index, columns=indexedByTime.columns)
+#resampled = indexedByTime.combine_first(dummy_frame).interpolate(method='time').resample('50ms').asfreq()
+
+resampled.to_csv('output2.tsv', sep='\t')
 
 
-print(cmds)
-print(cmds.index)
-print(cmds.columns)
 
-print(navData)
-print(navData.index)
-print(navData.columns)
-print(len(navDataColumnNames))
 
-print(merged)
-print(merged.index)
-print(merged.columns)
+
+
+
+
