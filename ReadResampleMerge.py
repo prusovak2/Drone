86d77640cec3447f3a2766dfab2e139a7141cd9a
prusovak2 <prusova.k2@seznam.cz>
2020-05-11 19:39:05 +0200

    new data read, resampled and merged, tabs problem solved

--- ReadResampleMerge.py
+++ ReadResampleMerge.py
@@ -13,7 +13,7 @@ def ReadCMDScvsIntoDFandResample(filename, frequency):
     resampledCmds = resampledCmds[cmds.index[0]:cmds.index[-1]]
     return resampledCmds
 
-def ReadNAVDATAcsvIntoDFandResample(filename, frequency, resampledCmds):
+def ReadNAVDATAcsvIntoDFandResample(filename, frequency):
     navDataColumnNames = ['time', 'State', 'Battery_level', 'Magnetometer_x', 'Magnetometer_y', 'Magnetometer_z',
                           'Pressure',
                           'Temperature', 'Wind_speed', 'Wind_angle', 'Wind_compensation:pitch',
@@ -24,10 +24,14 @@ def ReadNAVDATAcsvIntoDFandResample(filename, frequency, resampledCmds):
 
     navData = pd.read_csv(filename, delimiter='\t', header=None, usecols=range(1, 28),
                           names=navDataColumnNames)
-    # print("unique navdata time:", navData.time.is_unique)
+    # some debug print for adjusting function to new data files
+    # suffix = filename[12:]
+    # navData.to_csv("OutputStages\\tmpNav"+suffix, sep='\t')
+    print("unique navdata time:", navData.time.is_unique)
     # get rid of duplicated times in navData
     navData.drop_duplicates(['time'], keep='first', inplace=True)
-    # print("unique navdata time after .drop_duplicated call:", navData.time.is_unique)
+
+    print("unique navdata time after .drop_duplicated call:", navData.time.is_unique)
     navData.time = pd.to_datetime(navData.time, unit='ms')
     navData = navData.set_index('time')
 
@@ -36,12 +40,66 @@ def ReadNAVDATAcsvIntoDFandResample(filename, frequency, resampledCmds):
     resampledNav = resampledNav[navData.index[0]:navData.index[-1]]
     return resampledNav
 
-# READ AND RESAMPLE INPUT DATA
-resampledCmds = ReadCMDScvsIntoDFandResample('InputData\\commands.tsv', '50ms')
-resampledCmds.to_csv('OutputStages\\resampledCmds.tsv', sep='\t')
 
-resampledNav =ReadNAVDATAcsvIntoDFandResample('InputData\\navdata.tsv', '50ms', resampledCmds)
-resampledNav.to_csv('OutputStages\\resampledNav.tsv', sep='\t')
+def ReadResampleMerge(cmdsFilename, navdataFilename, frequency, cmdOutputTsvFilename=None, navdataOutputTsvFilename=None,
+                      mergedOutputTsvFilename=None):
+    '''
+    reads cmd and navdata files, resamples them to given frequency and results merges by ctime
+    whne output file names are given, prints corresponding outputs to .tsv files
+    :param cmdsFilename:
+    :param navdataFilename:
+    :param frequency:
+    :param cmdOutputTsvFilename:
+    :param navdataOutputTsvFilename:
+    :param mergedOutputTsvFilename:
+    :return:
+    '''
+    # READ AND RESAMPLE INPUT DATA
+    resampledCmds = ReadCMDScvsIntoDFandResample(cmdsFilename, frequency)
+    # print to .tsv file only when given a filename
+    if cmdOutputTsvFilename is not None:
+        resampledCmds.to_csv(cmdOutputTsvFilename, sep='\t')
+    resampledNav = ReadNAVDATAcsvIntoDFandResample(navdataFilename, frequency)
+    if navdataOutputTsvFilename is not None:
+        resampledNav.to_csv(navdataOutputTsvFilename, sep='\t')
+
+    # MERGE INPUT DATA
+    # how inner - intersection, keeps only times that do have corresponding counterpart in second file
+    merged = pd.merge(resampledCmds, resampledNav, right_index=True, left_index=True,
+                      how='inner')  # merge when time is index
+    # inputDF = pd.merge(cmds, navData, right_on="time", left_on="time", how='inner', indicator=True)
+    if mergedOutputTsvFilename is not None:
+        merged.to_csv(mergedOutputTsvFilename, sep='\t')
+    # print("unique time right after merge:", merged.index.is_unique)
+    return merged
+
+def addTabs(filename, outputFile):
+    with open(filename, mode='r') as input:
+        with open(outputFile, mode='w') as output:
+            for line in input:
+                output.write('\t'+line)
+
+merged = ReadResampleMerge('InputData\\commands.tsv', 'InputData\\navdata.tsv', '50ms', 'OutputStages\\resampledCmds.tsv',
+                           'OutputStages\\resampledNav.tsv', "OutputStages\\mergedResampled.tsv")
+
+# for some reason, a column of tabs is expected at the beginning of navdata file otherwise it cannot be parsed validly
+# add that column to navdataCM.tsv file
+addTabs('InputData\\navdataCM.tsv', 'InputData\\navdataCMTABS.tsv')
+
+mergedCM = ReadResampleMerge('InputData\\commandsCM.tsv', 'InputData\\navdataCMTABS.tsv', '50ms', 'OutputStages\\resampledCmdsCM.tsv',
+                           'OutputStages\\resampledNavCM.tsv', "OutputStages\\mergedResampledCM.tsv")
+
+# TODO: after this outputDF is indexed by senceless dates begginning with start of unix date - does it make any sence?
+# TODO: can I somehow format string representation of values in particular columns while printing them to file by to_scv
+# TODO: to print 'time' in some meaningfull format
+'''
+# ######################## DATASETS FOR CONFUSION MATRIX (MODEL VALIDATION) ############################################
+# READ AND RESAMPLE INPUT DATA
+resampledCmdsCM = ReadCMDScvsIntoDFandResample('InputData\\commandsCM.tsv', '50ms')
+resampledCmds.to_csv('OutputStages\\resampledCmdsCM.tsv', sep='\t')
+
+resampledNavCM =ReadNAVDATAcsvIntoDFandResample('InputData\\navdataCM.tsv', '50ms', resampledCmdsCM)
+resampledNav.to_csv('OutputStages\\resampledNavCM.tsv', sep='\t')
 
 # MERGE INPUT DATA
 # how inner - intersection, keeps only times that do have corresponding counterpart in second file
@@ -50,7 +108,4 @@ merged = pd.merge(resampledCmds, resampledNav, right_index=True, left_index=True
 
 merged.to_csv("OutputStages\\mergedResampled.tsv", sep='\t')
 # print("unique time right after merge:", merged.index.is_unique)
-
-# TODO: after this outputDF is indexed by senceless dates begginning with start of unix date - does it make any sence?
-# TODO: can I somehow format string representation of values in particular columns while printing them to file by to_scv
-# TODO: to print 'time' in some meaningfull format
+'''
