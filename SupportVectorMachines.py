74c2fd0d5b531531fe67f30ed90894df5db3d677
prusovak2 <prusova.k2@seznam.cz>
2020-09-09 16:56:25 +0200

    SupportVectorMachines finished

--- SupportVectorMachines.py
+++ SupportVectorMachines.py
@@ -14,7 +14,7 @@ from CreateDataMatrixForDT import dataForCM
 from BuildDT import GetLabel
 from BuildDT import GetFeatures
 
-
+"""
 def plotSVC(title,features,labels, svc):
 	y = labels[:]
 	X = features.values
@@ -34,40 +34,49 @@ def plotSVC(title,features,labels, svc):
 	plt.xlim(xx.min(), xx.max())
 	plt.title(title)
 	plt.show()
+"""
+
+def TuneParamsForSVM(labelColoumnName, dataForSVM):
+	labels = GetLabel(labelColoumnName, dataForSVM)
+	features = GetFeatures(dataForSVM)
+	svm_classifier = svm.SVC()
+	# pipe_steps = [('scaler', scaler), ('svm_classifier', svm_classifier)]
+	# pipeline = Pipeline(pipe_steps)
+	kernels = ['linear', 'rbf', 'poly']
+	params_to_try = [{'C': [1, 10, 100], 'kernel': kernels, 'gamma': [1, 10, 30, 50, 70, 100]}]
+	x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.20,
+														stratify=labels, random_state=42)
+	scaler = StandardScaler()
+	x_train = scaler.fit_transform(x_train)
+	y_train = np.ravel(y_train)
+
+	score = 0
+	for cv in range(3, 6):
+		create_grid = GridSearchCV(svm_classifier, param_grid=params_to_try, cv=cv)
+		create_grid.fit(x_train, y_train)
+		newScore = create_grid.score(x_test, y_test)
+		print("score for %d fold cross validation is %3.2f" % (cv, newScore))
+		print("best fit params:")
+		currentBestParams = create_grid.best_params_
+		print(currentBestParams)
+		if newScore > score:
+			score = newScore
+			bestParams = currentBestParams
+			bestEstimator = create_grid.best_estimator_
+	return bestEstimator, scaler
 
 
-labels = GetLabel('leftRight', dataForSVM)
-features = GetFeatures(dataForSVM)
-svm_classifier = svm.SVC()
-# pipe_steps = [('scaler', scaler), ('svm_classifier', svm_classifier)]
-# pipeline = Pipeline(pipe_steps)
-kernels = ['linear', 'rbf', 'poly']
-params_to_try = [{'C': [1, 10, 100], 'kernel': kernels, 'gamma': [1, 10, 30, 50,70, 100]}]
-x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.20,
-													stratify=labels, random_state=42)
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-y_train = np.ravel(y_train)
+if __name__ == "__main__":
+	leftRightEstimator, leftRightScaler = TuneParamsForSVM('leftRight', dataForSVM)
+	CreateConfusionMatrix('leftRight', dataForCM, leftRightEstimator, 'leftRightSVM', plt.cm.Reds, leftRightScaler)
 
-score = 0
-for cv in range(3, 6):
-	create_grid = GridSearchCV(svm_classifier, param_grid=params_to_try, cv=cv)
-	create_grid.fit(x_train, y_train)
-	newScore = create_grid.score(x_test, y_test)
-	print("score for %d fold cross validation is %3.2f" % (cv, newScore))
-	print("best fit params:")
-	currentBestParams = create_grid.best_params_
-	print(currentBestParams)
-	if newScore > score:
-		score = newScore
-		bestParams = currentBestParams
-		bestEstimator =create_grid.best_estimator_
+	frontBackEstimator, frontBackScaler = TuneParamsForSVM('frontBack', dataForSVM)
+	CreateConfusionMatrix('frontBack', dataForCM, frontBackEstimator, 'frontBackSVM', plt.cm.Reds, frontBackScaler)
 
-CreateConfusionMatrix('leftRight', dataForCM, bestEstimator, 'firstSVMCM', plt.cm.Reds, scaler)
+	angularEstimator, angularScaler = TuneParamsForSVM('angular', dataForSVM)
+	CreateConfusionMatrix('angular', dataForCM, angularEstimator, 'angularSVM', plt.cm.Reds, angularScaler)
 
-baseSVM = svm.SVC()
-baseSVM.fit(x_train, y_train)
-CreateConfusionMatrix('leftRight', dataForCM, baseSVM, 'baseSVM', plt.cm.Reds, scaler)
+
 
 """
 # import some data to play with
